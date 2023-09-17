4d061c11440f896a18a2178170fd97e65209ba01
Danys Guevara <ga19014@ues.edu.sv>
2023-09-16 20:32:21 -0600

    Inicio del proyecto

---
+++ src/main/java/com/hotelgloria/apiHotelGloria/ApiHotelGloriaApplication.java
@@ -0,0 +1,13 @@
+package com.hotelgloria.apiHotelGloria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHotelGloriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHotelGloriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotelgloria/apiHotelGloria/ApiHotelGloriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelgloria.apiHotelGloria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHotelGloriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
